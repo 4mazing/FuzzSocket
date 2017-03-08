9e80e63133c7e7741cf80a2f5d0426d2c66c3ef2
yizhimanpadewoniu@yeah.net <yizhimanpadewoniu@yeah.net>
2017-03-08 14:54:11 +0800

    change the argvs when you run it

--- fuzz_socket.py
+++ fuzz_socket.py
@@ -5,9 +5,9 @@ import time
 import string
 import os
 
-hello = "\x03\x00\x00\x16\x11\xe0\x00\x00\x00\x12\x00\xc1\x02\x01\x00\xc2\x02\x01\x02\xc0\x01\x0a"
-set_comm = "\x03\x00\x00\x19\x02\xf0\x80\x32\x01\x00\x00\x00\x00\x00\x08\x00\x00\xf0\x00\x00\x01\x00\x01\x01\xe0"
-message = "\x03\x00\x00\x21\x02\xf0\x80\x32\x07\x00\x00\x01\x00\x00\x08\x00\x08\x00\x01\x12\x04\x11\x44\x01\x00\xff\x09\x00\x04\x00\x11\x00\x00"
+hello = "0300001611e00000001200c1020100c2020102c0010a"
+set_comm = "0300001902f08032010000000000080000f0000001000101e0"
+message = "0300002102f080320700000100000800080001120411440100ff09000400110000"
 message_str = "0300002102f080320700000100000800080001120411440100ff09000400110000"
 userdata_dict = {0:'01', 1:'02', 2:'03', 3:'07'}
 subfunction_dict = {0:'01', 1:'02', 2:'0c', 3:'0e', 4:'0f', 5:'10', 6:'13'}
@@ -71,11 +71,11 @@ while True:
     replay = str2byte(replay_pkt)
     cliconn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     cliconn.connect((host, port))
-    cliconn.send(hello)
+    cliconn.send(str2byte(hello))
     time.sleep(1)
-    cliconn.send(set_comm)
+    cliconn.send(str2byte(set_comm))
     time.sleep(1)
-    cliconn.send(replay)
+    cliconn.send(str2byte(replay))
     os.system('ping 172.18.15.108 -n 1 -w 3')
     if cliconn.recv == '':
         logfile.write("%s\n%s\n\n" %(localtime(), replay_pkt))
